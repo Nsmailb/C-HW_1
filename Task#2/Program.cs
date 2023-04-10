07aae61ccb3f0984b3b68caff2f67a9f2d64701a
Ivan M <nsmailb79@gmail.com>
2023-04-10 20:28:44 +0400

    programm for Task2

--- Task#2/Program.cs
+++ Task#2/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Какое число больше - первое или второе?");
+Console.WriteLine("Ведите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 >= number2)
+{
+Console.WriteLine("Первое число больше второго (или равно ему, если вы ввели одинаковые числа)!");
+}
+else 
+{
+Console.WriteLine("Первое число меньше второго второго!");
+}
+
+Console.Write("The End");
