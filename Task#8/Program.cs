99d64a74952084370eb920198621ea56c7958334
Ivan M <nsmailb79@gmail.com>
2023-04-11 12:15:04 +0400

    added all in Task#8

---
+++ Task#8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("При вводе числе показываю все четные от 1 до введенного");
+Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i <= number)
+    {
+    if((i % 2) == 0)    
+        {
+            Console.WriteLine(i);
+            i=i+i;
+        }
+   Console.WriteLine(i++);
+   }
+Console.Write("The End");
