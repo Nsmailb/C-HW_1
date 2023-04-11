8f64480b03dbdcd908716813226d65c6f55f7f96
Ivan M <nsmailb79@gmail.com>
2023-04-11 06:40:53 +0400

    added all in Task#6

---
+++ Task#6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Является ли ваше число четным?");
+Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number % 2) == 0)
+Console.WriteLine("Ваше число четное!");
+else 
+{
+Console.WriteLine("Ваше число не четное!");
+}
+Console.Write("The End");
