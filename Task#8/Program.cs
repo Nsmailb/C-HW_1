961f931ade99f0e84396b5a5585fb5b3ce26942c
Ivan M <nsmailb79@gmail.com>
2023-04-12 16:22:13 +0400

    added programm for task#8

--- Task#8/Program.cs
+++ Task#8/Program.cs
@@ -1,15 +1,30 @@
-﻿Console.WriteLine("При вводе числе показываю все четные от 1 до введенного");
+﻿// Вариант А
+// Console.WriteLine("При вводе числа показываю все четные числа от 1 до введенного");
+// Console.WriteLine("Ведите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while(i<=number)
+// {
+//         if(i % 2 == 0)
+//         {
+//         Console.Write(i+", ");
+
+//         }
+//         i++;    
+// }
+
+//Вариант Б
+Console.WriteLine("При вводе числа показываю все четные числа от 1 до введенного");
 Console.WriteLine("Ведите число");
 int number = Convert.ToInt32(Console.ReadLine());
-int i = 1;
-
-while(i <= number)
-    {
-    if((i % 2) == 0)    
+int i = 2;
+while(i<=number)
+{
         {
-            Console.WriteLine(i);
-            i=i+i;
+        Console.WriteLine(i+" ");
+
         }
-   Console.WriteLine(i++);
-   }
-Console.Write("The End");
+        i=i+2;    
+}
+
+Console.WriteLine("The End");
