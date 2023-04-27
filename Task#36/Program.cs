777faa0d83f81a8eb90537f30bce06ff22f1cab3
Ivan M <nsmailb79@gmail.com>
2023-04-27 15:53:33 +0400

    added Tasks 34; 36; 38

---
+++ Task#36/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+///[3, 7, 23, 12] -> 19 
+///[-4, -6, 89, 6] -> 0
+
+int[] numArr = new int [10];
+int SumNotEvenPos = 0;
+
+for (int i=0; i<numArr.Length; i++)
+    {
+    numArr[i] = new Random().Next(0, 10);
+    if ((i % 2) != 0)
+    {
+    SumNotEvenPos+=numArr[i];
+    }
+}    
+Console.WriteLine("Массив чисел: "+String.Join(", ", numArr));
+Console.WriteLine("Сумма чисел массива на нечетных позициях (i) ="+" "+SumNotEvenPos);
---
+++ Task#38/Program.cs
@@ -0,0 +1,43 @@
+﻿// ///Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// ///[3 7 22 2 78] -> 76
+
+int[] numArr = new int [10];
+int Diff = 0;
+
+for (int i = 0; i < numArr.Length; i++)
+{
+    numArr[i] = new Random().Next(-10, 10);
+}
+Console.WriteLine("Массив чисел: "+String.Join(", ", numArr));
+
+int Max = numArr[0];
+int Min = numArr[0];
+
+int GetMax()
+{
+   for (int i = 0; i < numArr.Length; i++)
+    {
+        if (numArr[i]>Max)
+        {
+           Max=numArr[i];
+        }
+    }
+    return Max;
+}
+int ResMax = GetMax();
+Console.WriteLine("Максимальное число ="+" "+ResMax);
+
+int GetMin()
+{
+    for (int i = 0; i < numArr.Length; i++)
+    {
+        if (numArr[i] < Min)
+        {
+           Min=numArr[i];
+        }
+    }
+    return Min;
+}
+int ResMin = GetMin();
+Console.WriteLine("Минимальное число ="+" "+ResMin);
+Console.WriteLine("Разница чисел: "+(Diff=Max-Min));
