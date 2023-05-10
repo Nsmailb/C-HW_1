7f02236b2d6095b8e767ae4e9baed9f6cb6667c3
Ivan M <nsmailb79@gmail.com>
2023-05-10 09:30:29 +0400

    added Tasks 41; 43

---
+++ Task#41/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+Console.WriteLine("Введите число <> 0");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число <> 0");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число <> 0");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число <> 0");
+int num4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число <> 0");
+int num5 = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = new int [5];
+Array[0] = num1;
+Array[1] = num2;
+Array[2] = num3;
+Array[3] = num4;
+Array[4] = num5;
+int MoreThenZero = 0;
+Console.WriteLine("Вы ввели числа: "+String.Join(", ", Array));
+
+for (int i=0; i<Array.Length; i++)
+    {
+    if ((Array[i] > 0))
+    {
+    MoreThenZero=MoreThenZero+1;
+    }
+    }
+Console.WriteLine("Ровно "+MoreThenZero+""+" из них больше 0.");
---
+++ Task#43/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями -
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// Пример: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите первую координату прямой A(k1)");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую координату прямой A(b1)");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первую координату прямой B(k2)");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую координату прямой B(b2)");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double X = 0;
+double Y = 0;
+if (k1!=k2)
+{
+        X = (b2 - b1) / (k1 - k2);
+        Y = k1 * X+ b1;
+    Console.WriteLine("Координаты пересечения прямых по оси X"+" "+X); 
+    Console.WriteLine("Координаты пересечения прямых по оси Y"+" "+Y);
+}
+else
+{
+    Console.WriteLine("Прямые не пересекаюся.");
+}
